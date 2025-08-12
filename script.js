d07f8b5746e3141e3b62f58926f8234ad73616b1
Kauan-Martins09 <kauanmartinsesilva@gmail.com>
2025-08-11 22:32:09 -0300

    save

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Acessa o botão e o parágrafo pelo ID
+const meuBotao = document.getElementById('meuBotao');
+const mensagem = document.getElementById('mensagem');
 
+// Adiciona um "ouvinte de evento" (event listener) de clique ao botão
+meuBotao.addEventListener('click', function() {
+    // Quando o botão é clicado, muda o conteúdo do parágrafo
+    mensagem.textContent = 'O texto foi alterado com sucesso pelo JavaScript!';
+});
